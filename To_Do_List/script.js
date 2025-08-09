e69f96d09db98e4bec71d20cc6c177aae27666e4
Devraj-jha <devrajjha100806@gmail.com>
2025-08-09 12:09:25 +0530

    added a add button in to do list project

--- To_Do_List/script.js
+++ To_Do_List/script.js
@@ -1,1 +1,23 @@
-const Input = document.getElementById("todovalue")
+//first thing to do is to take elements from html and store them in varible.
+
+const taskinput = document.getElementById("taskinput")
+const tasklist =  document.getElementById("tasklist")
+const addtask = document.getElementById("addtask")
+
+function addTask() {
+
+    const text = taskinput.value.trim();
+    if(!text) retun;
+
+
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+
+    span.textContent = text;
+    li.appendChild(span);
+
+    tasklist.appendChild(li);
+    taskinput.value = '';
+    taskinput.focus();
+}
+  addtask.addEventListener('click', addTask);
